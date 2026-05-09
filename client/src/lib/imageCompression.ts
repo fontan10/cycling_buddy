882b1362c367ca1cba7d4893aa4aa5d011841563
Joseph Neil Fontanilla <josephfontanilla2@gmail.com>
2026-05-09 13:26:16 +0300

    fix compression logic

--- client/src/lib/imageCompression.ts
+++ client/src/lib/imageCompression.ts
@@ -3,5 +3,5 @@ export function getCompressionOptions() {
   if (memory <= 1) return { maxSizeMB: 0.3, maxWidthOrHeight: 640,  useWebWorker: false, fileType: 'image/jpeg' }
   if (memory <= 2) return { maxSizeMB: 0.5, maxWidthOrHeight: 800,  useWebWorker: false, fileType: 'image/jpeg' }
   if (memory <= 4) return { maxSizeMB: 1.0, maxWidthOrHeight: 1024, useWebWorker: false, fileType: 'image/jpeg' }
-  return               { maxSizeMB: 1.5, maxWidthOrHeight: 1280, useWebWorker: true,  fileType: 'image/jpeg' }
+  return               { maxSizeMB: 1.5, maxWidthOrHeight: 1280, useWebWorker: false, fileType: 'image/jpeg' }
 }
